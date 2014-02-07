9484753997892431b29f3d1a4b33c13db69b2e4f
Jack <xhzengAIB@gmail.com>
2014-02-07 16:24:02 +0000

    Add project

---
+++ XHPathCover/XHPathCover/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XHPathCover
+//
+//  Created by 曾 宪华 on 14-2-7.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
