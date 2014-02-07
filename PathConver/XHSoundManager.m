023e2544a5711c1317416e527884847b4a2dcfe1
Jack <xhzengAIB@gmail.com>
2014-02-07 16:41:41 +0000

    init XHSoundManager

---
+++ PathConver/XHSoundManager.m
@@ -0,0 +1,31 @@
+//
+//  XHSoundManager.m
+//  XHPathCover
+//
+//  Created by 曾 宪华 on 14-2-7.
+//  Copyright (c) 2014年 曾宪华 开发团队(http://iyilunba.com ) 本人QQ:543413507 本人QQ群（142557668）. All rights reserved.
+//
+
+#import "XHSoundManager.h"
+
+@implementation XHSoundManager
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
