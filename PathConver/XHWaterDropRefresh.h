05e400f86da45b4fe4ec2657125207379ef1f035
Jack <xhzengAIB@gmail.com>
2014-02-07 16:42:28 +0000

    design XHWaterDropRefresh api

--- PathConver/XHWaterDropRefresh.h
+++ PathConver/XHWaterDropRefresh.h
@@ -10,4 +10,17 @@
 
 @interface XHWaterDropRefresh : UIView
 
+@property (nonatomic, assign) CGFloat radius; // default is 5.
+@property (nonatomic, assign) CGFloat maxOffset; // default is 70
+@property (nonatomic, assign) CGFloat deformationLength; // default is 0.4 (between 0.1 -- 0.9)
+@property (nonatomic, assign) CGFloat offsetHeight;
+@property (nonatomic, strong) UIImage *refreshCircleImage;
+@property (nonatomic, readonly) BOOL isRefreshing;
+
+- (void)stopRefresh;
+- (void)startRefreshAnimation;
+
+@property (nonatomic, copy) void(^handleRefreshEvent)(void) ;
+@property (nonatomic) float currentOffset;
+
 @end
